3afaf72f027618a4d74fb361ad3628a9e7e0955c
Aditya <165382258+AdityaDcode@users.noreply.github.com>
2024-12-29 10:18:33 +0530

    Update app.js

--- Javascript Projects/BlurryImg/Starter/app.js
+++ Javascript Projects/BlurryImg/Starter/app.js
@@ -12,6 +12,7 @@ function blurring(){
     loadText.style.opacity = scale(load,0,100,1,0);
     bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`;
 }
+//Function to map range of numbers to another given by stackoverflow
 function scale (number, inMin, inMax, outMin, outMax) {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
